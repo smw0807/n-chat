0a8c7338674b60c401ecfc0ff9e5132dfdfa149b
smw0807 <smw0807@gmail.com>
2025-08-17 16:04:47 +0900

    cors config module

--- backend/src/config/config.module.ts
+++ backend/src/config/config.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule as NestConfigModule } from '@nestjs/config';
 
 import appConfig from './conf/app.config';
 import authConfig from './conf/auth.config';
+import corsConfig from './conf/cors.config';
 
 import { validationSchema } from './validation.schema';
 
@@ -10,7 +11,7 @@ import { validationSchema } from './validation.schema';
   imports: [
     NestConfigModule.forRoot({
       envFilePath: process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env',
-      load: [appConfig, authConfig],
+      load: [appConfig, authConfig, corsConfig],
       isGlobal: true,
       validationSchema: validationSchema,
     }),
