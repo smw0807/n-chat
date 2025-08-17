9ebadc6be6eb8581872ba4b87d167dda28fd3acc
smw0807 <smw0807@gmail.com>
2025-08-17 16:04:06 +0900

    logLevel 환경변수 추가

--- backend/src/config/conf/app.config.ts
+++ backend/src/config/conf/app.config.ts
@@ -2,4 +2,5 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('app', () => ({
   appName: process.env.APP_NAME,
   appPort: process.env.APP_PORT,
+  logLevel: process.env.LOG_LEVEL,
 }));
