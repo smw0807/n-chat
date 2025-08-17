b6ae037f9cdf19e72b16a0a758852b122f7b4f8c
smw0807 <smw0807@gmail.com>
2025-08-17 16:06:02 +0900

    prefix, cors, app port 수정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { LoggerService } from './logger/logger.service';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useLogger(LoggerService);
+
+  app.setGlobalPrefix('api');
+
+  const configService = app.get(ConfigService);
+
+  const corsConfig = configService.get('cors') as {
+    origin: string;
+    methods: string;
+    allowedHeaders: string;
+  };
+
+  app.enableCors({
+    origin: corsConfig.origin,
+    methods: corsConfig.methods,
+    allowedHeaders: corsConfig.allowedHeaders,
+  });
+
+  const appConfig = configService.get('app');
+
+  LoggerService.log(`[ Server is running on port ${appConfig.appPort} ]`);
+  await app.listen(appConfig.appPort ?? 3000);
 }
 bootstrap();
