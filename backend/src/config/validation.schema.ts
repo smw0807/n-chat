a5767678ce4286c75a321be28c3d9b8efe1b98ff
smw0807 <smw0807@gmail.com>
2025-08-17 16:05:34 +0900

    config validation 추가

--- backend/src/config/validation.schema.ts
+++ backend/src/config/validation.schema.ts
@@ -3,6 +3,10 @@ import * as Joi from 'joi';
 export const validationSchema = Joi.object({
   APP_NAME: Joi.string().required(),
   APP_PORT: Joi.number().required(),
+  LOG_LEVEL: Joi.string().required(),
   BCRYPT_SALT: Joi.string().required(),
   JWT_SECRET: Joi.string().required(),
+  CORS_ORIGIN: Joi.string().required(),
+  CORS_METHODS: Joi.string().required(),
+  CORS_ALLOWED_HEADERS: Joi.string().required(),
 });
