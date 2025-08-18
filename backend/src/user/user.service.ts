25f8b885fed4203d50c5a41847f32cb37548731c
smw0807 <smw0807@gmail.com>
2025-08-18 13:20:05 +0900

    사용자

---
+++ backend/src/user/user.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entity/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.find();
+  }
+
+  async findOne(id: string): Promise<User | null> {
+    return this.userRepository.findOne({ where: { id } });
+  }
+}
