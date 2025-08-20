ef0fd418a26cb31159dad07392fdf35f8c9e066d
smw0807 <smw0807@gmail.com>
2025-08-20 16:46:32 +0900

    오류 수정

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
 import { UserService } from './user.service';
@@ -7,7 +7,11 @@ import { UtilsModule } from 'src/utils/utils.module';
 import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), UtilsModule, AuthModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    UtilsModule,
+    forwardRef(() => AuthModule),
+  ],
   providers: [UserService],
   controllers: [UserController],
   exports: [UserService],
