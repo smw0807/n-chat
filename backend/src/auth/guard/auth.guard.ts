9eedf575b1fa407ad785925c51bef688b9f9278c
smw0807 <smw0807@gmail.com>
2025-08-21 09:52:04 +0900

    AuthGuard 예외처리

--- backend/src/auth/guard/auth.guard.ts
+++ backend/src/auth/guard/auth.guard.ts
@@ -2,27 +2,34 @@ import {
   CanActivate,
   ExecutionContext,
   Injectable,
+  Logger,
   UnauthorizedException,
 } from '@nestjs/common';
 import { AuthService } from '../auth.service';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  private readonly logger = new Logger(AuthGuard.name);
   constructor(private readonly authService: AuthService) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const request = context.switchToHttp().getRequest();
-    const token = request.headers.authorization;
+    try {
+      const request = context.switchToHttp().getRequest();
+      const token = request.headers.authorization;
 
-    if (!token) {
-      throw new UnauthorizedException('토큰이 없습니다.');
-    }
-    const tokenValue = token.split(' ')[1];
-    const decoded = await this.authService.verifyToken(tokenValue);
-    if (!decoded) {
+      if (!token) {
+        throw new UnauthorizedException('토큰이 없습니다.');
+      }
+      const tokenValue = token.split(' ')[1];
+      const decoded = await this.authService.verifyToken(tokenValue);
+      if (!decoded) {
+        throw new UnauthorizedException('유효하지 않은 토큰입니다.');
+      }
+      request.user = decoded;
+      return true;
+    } catch (e) {
+      this.logger.error(e);
       throw new UnauthorizedException('유효하지 않은 토큰입니다.');
     }
-    request.user = decoded;
-    return true;
   }
 }
