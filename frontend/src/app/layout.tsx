71b65edde25f266ea491752cd910365bd5dd78e8
smw0807 <smw0807@gmail.com>
2025-08-23 15:35:16 +0900

    createPortal을 위한 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -18,6 +18,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body>
+        <div id="modal"></div>
         <CookiesProvider>{children}</CookiesProvider>
         {modal}
       </body>
