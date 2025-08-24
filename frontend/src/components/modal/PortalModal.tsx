fb10d9df145f5e2298a13cd36d920cd070010173
smw0807 <smw0807@gmail.com>
2025-08-24 15:13:13 +0900

    포탈모달 수정

--- frontend/src/components/modal/PortalModal.tsx
+++ frontend/src/components/modal/PortalModal.tsx
@@ -5,8 +5,9 @@ import { createPortal } from 'react-dom';
 interface ModalProps {
   isOpen: boolean;
   title?: string;
-  content?: string;
+  content?: string | React.ReactNode;
   size?: 'sm' | 'md' | 'lg' | 'xl';
+  footer?: React.ReactNode;
   setIsOpen: (isOpen: boolean) => void;
 }
 
@@ -16,6 +17,7 @@ function PortalModal({
   size = 'md',
   isOpen,
   setIsOpen,
+  footer,
 }: ModalProps) {
   const sizeClasses = {
     sm: 'max-w-sm',
@@ -41,14 +43,19 @@ function PortalModal({
         {/* Content */}
         <div className="p-6">{content}</div>
 
-        <div className="flex justify-end gap-4">
-          <button
-            className="bg-gray-500 text-white px-4 py-2 rounded-md"
-            onClick={() => setIsOpen(false)}
-          >
-            확인
-          </button>
-        </div>
+        {/* Footer */}
+        {footer ? (
+          footer
+        ) : (
+          <div className="flex justify-end gap-4">
+            <button
+              className="bg-gray-500 text-white px-4 py-2 rounded-md"
+              onClick={() => setIsOpen(false)}
+            >
+              확인
+            </button>
+          </div>
+        )}
       </div>
     </div>,
     document.getElementById('modal') as HTMLElement
