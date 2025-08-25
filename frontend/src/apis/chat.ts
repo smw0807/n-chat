d113158e606d0f3341a702f483e0809d7fc4fc60
smw0807 <smw0807@gmail.com>
2025-08-25 14:01:23 +0900

    방정보 API 주소

--- frontend/src/apis/chat.ts
+++ frontend/src/apis/chat.ts
@@ -3,3 +3,6 @@ import { API_BASE_URL } from '@/constants/api';
 export const getRooms = `${API_BASE_URL}/chat/rooms`;
 
 export const createRoom = `${API_BASE_URL}/chat/rooms`;
+
+export const getRoomInfo = (roomId: number) =>
+  `${API_BASE_URL}/chat/rooms/${roomId}`;
