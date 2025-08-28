ea54641ce6117982e3ccc6b1ee1717d7029dc501
smw0807 <smw0807@gmail.com>
2025-08-28 15:06:10 +0900

    로그인 해야 방생성 가능하게 수정

--- frontend/src/components/MainButtons.tsx
+++ frontend/src/components/MainButtons.tsx
@@ -1,12 +1,16 @@
 'use client';
 import { useState } from 'react';
 import useRooms from '@/hooks/useRooms';
+import useAuth from '@/hooks/useAuth';
+
 import Modal from '@/components/modal/PortalModal';
 import CreateRoom from './form/CreateRoom';
 
 function MainButtons() {
   const { fetchRooms } = useRooms();
   const [isOpen, setIsOpen] = useState(false);
+  const [hasUser, setHasUser] = useState(false);
+  const { user } = useAuth();
 
   return (
     <div className="flex justify-end gap-4">
@@ -21,8 +25,13 @@ function MainButtons() {
       {/* 방만들기 버튼 */}
       <button
         className="bg-blue-500 text-white px-4 py-2 text-sm rounded-md"
-        onClick={() => setIsOpen(true)}
-        // onClick={() => router.push('/createRoom')}
+        onClick={() => {
+          if (user) {
+            setIsOpen(true);
+          } else {
+            setHasUser(true);
+          }
+        }}
       >
         방만들기
       </button>
@@ -31,6 +40,20 @@ function MainButtons() {
         setIsOpen={setIsOpen}
         title="방만들기"
         content={<CreateRoom setIsOpen={setIsOpen} />}
+      />
+      <Modal
+        isOpen={hasUser}
+        setIsOpen={setHasUser}
+        title="방만들기"
+        content={<div className="text-center">로그인 후 이용해주세요.</div>}
+        footer={
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded-md"
+            onClick={() => setHasUser(false)}
+          >
+            확인
+          </button>
+        }
       />
     </div>
   );
