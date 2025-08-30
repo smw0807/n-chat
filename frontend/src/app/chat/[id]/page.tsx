0d2e1770efabf47bd7676bebd8acf53e5dde6e75
smw0807 <smw0807@gmail.com>
2025-08-30 18:14:30 +0900

    삭제

--- frontend/src/app/chat/[id]/page.tsx
+++ frontend/src/app/chat/[id]/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import React, { useState, useEffect, useRef } from 'react';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { io } from 'socket.io-client';
 import dayjs from 'dayjs';
